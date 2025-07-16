2ddd9086109cd621fbd9ee269a1c440e1550d5fd
ivanst101 <ivanstojkovic177@gmail.com>
2025-07-16 12:48:02 +0200

    Update server.js file with routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,23 @@
 import express from "express";
+const port = 5001;
 const app = express();
 
-app.listen(5001, () => {
-  console.log("Listening on port 5001");
+app.get("/api/notes", (req, res) => {
+  res.status(200).send("You got 10 notes");
+});
+
+app.post("/api/notes", (req, res) => {
+  res.status(201).json({ message: "Post created successfully" });
+});
+
+app.put("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Post updated successfully" });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Post deleted successfully" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
