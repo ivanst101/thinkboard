40b8e9f78edb6f1e1e33fadd126bd3710e39d4f6
ivanst101 <ivanstojkovic177@gmail.com>
2025-07-16 14:56:00 +0200

    Add and connect database: MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 import express from "express";
-const port = 5001;
-const app = express();
 import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5001;
+const app = express();
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
