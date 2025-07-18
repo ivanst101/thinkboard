5025276dcf7675224250153dd06486c483c8cb45
ivanst101 <ivanstojkovic177@gmail.com>
2025-07-18 21:01:13 +0200

    Update: CreatePage and axios requests

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import axios from "axios";
+import api from "../lib/axios.js";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
 
@@ -11,7 +11,7 @@ function HomePage() {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
         setNotes(res.data);
       } catch (error) {
         console.error(error);
